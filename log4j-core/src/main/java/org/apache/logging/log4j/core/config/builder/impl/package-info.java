7c6308d5d90fd3d668b6b1f2a001ea66db75445d
Jeff Thomas <jeff.w.thomas@outlook.com>
2025-03-01 14:15:26 +0100

    Reworked the builder.api and builder.api.impl packages (#2791)
    + made all Default***Builder implementations @ProviderrType
    + added setXXXAttribute method to some component builder interfaces with default implementations
    + added JSpecify @NullMarked to packages and @Nullable where approrpiate to fields/mehods (per last PR review)
    + deprecated ComponentBuilder#addAttribute(...) methods and replaced with ComponentBuilder#setAttribute(...) methods in line with other builders (i.e. Plugins) - also since they don't just add but also remove if the value is null
    + reorganized ConfigurationBuilder to group getters, setters, builder factory methods
    + incremented package-info.java @Version annoations
    + added some null-guards in BuiltConfiguration#setup()
    + moved DeffaultConfigurationBuilder XML generation code from top to bottom of source file
    + removed "extra" attributes in Default***Builder constructors and replaced with setXXXAttribute chained calls in DefaultConfigurrationBuilder#newXXX(...)  convenience methods
    + javadoc'd both packages (fixing documentation errors)

--- log4j-core/src/main/java/org/apache/logging/log4j/core/config/builder/impl/package-info.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/config/builder/impl/package-info.java
@@ -21,7 +21,9 @@
  */
 @Export
 @Version("2.25.3")
+@NullMarked
 package org.apache.logging.log4j.core.config.builder.impl;
 
+import org.jspecify.annotations.NullMarked;
 import org.osgi.annotation.bundle.Export;
 import org.osgi.annotation.versioning.Version;
--- log4j-core/src/main/java/org/apache/logging/log4j/core/config/properties/PropertiesConfigurationBuilder.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/config/properties/PropertiesConfigurationBuilder.java
@@ -365,7 +365,7 @@ public class PropertiesConfigurationBuilder extends ConfigurationBuilderFactory
             final ConfigurationBuilder<?> parentBuilder, final String key, final Properties properties) {
         final String name = (String) properties.remove(CONFIG_NAME);
         final String type = (String) properties.remove(CONFIG_TYPE);
-        if (Strings.isEmpty(type)) {
+        if (type == null || Strings.isEmpty(type)) {
             throw new ConfigurationException("No type attribute provided for component " + key);
         }
         final ComponentBuilder<B> componentBuilder = parentBuilder.newComponent(name, type);
